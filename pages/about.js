7b51678f60d6fd3f92d3cc93f47b069dd1645571
Kazuki Ochiai <kazu910739@gmail.com>
2022-12-09 23:13:27 +0900

    3rd chapter changes commit

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Footer } from '../components/footer'
 import { Links } from '../components/Links'
+import Headline from '../components/Headline'
 
 export default function Home() {
   return (
@@ -15,13 +15,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/about.js</code>
-        </p>
+        <Headline page="about" />
         <Links />
       </main>
 
